c27e8e2db2dbcfd46d007df2b8e3907037016d0d
Jesús Antonio Torres F <97558737+iAntonAMC@users.noreply.github.com>
2022-04-08 16:54:47 +0000

    -Se agrega la funciòn movDerecha a la versiòn beta

--- newsokoban.py
+++ newsokoban.py
@@ -52,3 +52,73 @@ class Sokoban:
             elif 5 in linea:
                 self.posy = self.map.index(linea)
                 self.posx = linea.index(5)
+
+    def movDerecha(self):
+        #Muñeco, Espacio
+        if self.map[self.posy][self.posx] == 1 and self.map[self.posy][self.posx + 1] == 0:
+            self.map[self.posy][self.posx] = 0
+            self.map[self.posy][self.posx + 1] = 1
+            self.posx += 1
+        #Muñeco, Meta
+        elif self.map[self.posy][self.posx] == 1 and self.map[self.posy][self.posx + 1] == 4:
+            self.map[self.posy][self.posx] = 0
+            self.map[self.posy][self.posx + 1] = 5
+            self.posx += 1
+        #Muñeco, Caja, Espacio
+        elif self.map[self.posy][self.posx] == 1 and self.map[self.posy][self.posx + 1] == 2 and self.map[self.posy][self.posx + 2] == 0:
+            self.map[self.posy][self.posx] = 0
+            self.map[self.posy][self.posx + 1] = 1
+            self.map[self.posy][self.posx + 2] = 2
+            self.posx += 1
+        #Muñeco, Caja, Meta
+        elif self.map[self.posy][self.posx] == 1 and self.map[self.posy][self.posx + 1] == 2 and self.map[self.posy][self.posx + 2] == 4:
+            self.map[self.posy][self.posx] = 0
+            self.map[self.posy][self.posx + 1] = 1
+            self.map[self.posy][self.posx + 2] = 6
+            self.posx += 1
+        #Muñeco-meta,  Espacio
+        elif self.map[self.posy][self.posx] == 5 and self.map[self.posy][self.posx + 1] == 0:
+            self.map[self.posy][self.posx] = 4
+            self.map[self.posy][self.posx + 1] = 1
+            self.posx += 1
+        #Muñeco-meta, Meta
+        elif self.map[self.posy][self.posx] == 5 and self.map[self.posy][self.posx + 1] == 4:
+            self.map[self.posy][self.posx] = 4
+            self.map[self.posy][self.posx + 1] = 5
+            self.posx += 1
+        #Muñeco-meta,  Caja, Espacio
+        elif self.map[self.posy][self.posx] ==  5 and self.map[self.posy][self.posx + 1] == 2 and self.map[self.posy][self.posx + 2] == 0:
+            self.map[self.posy][self.posx] = 4
+            self.map[self.posy][self.posx + 1] = 1
+            self.map[self.posy][self.posx + 2] = 2
+            self.posx += 1
+        #Muñeco-meta, Caja, Meta
+        elif self.map[self.posy][self.posx] == 5 and self.map[self.posy][self.posx + 1] == 2 and self.map[self.posy][self.posx + 2] == 4:
+            self.map[self.posy][self.posx] = 4
+            self.map[self.posy][self.posx + 1] = 1
+            self.map[self.posy][self.posx + 2] = 6
+            self.posx += 1
+        #Muñeco-meta, Caja-meta, Espacio
+        elif self.map[self.posy][self.posx] == 5 and self.map[self.posy][self.posx + 1] == 6 and self.map[self.posy][self.posx + 2] == 0:
+            self.map[self.posy][self.posx] = 4
+            self.map[self.posy][self.posx + 1] = 5
+            self.map[self.posy][self.posx + 2] = 2
+            self.posx += 1
+        #Muñeco-meta, Caja-meta, Meta
+        elif self.map[self.posy][self.posx] == 5 and self.map[self.posy][self.posx + 1] == 6 and self.map[self.posy][self.posx + 2] == 4:
+            self.map[self.posy][self.posx] = 4
+            self.map[self.posy][self.posx + 1] = 5
+            self.map[self.posy][self.posx + 2] = 6
+            self.posx += 1
+        #Muñeco, Caja-meta, Espacio
+        elif self.map[self.posy][self.posx] == 1 and self.map[self.posy][self.posx + 1] == 6 and self.map[self.posy][self.posx + 2] == 0:
+            self.map[self.posy][self.posx] = 0
+            self.map[self.posy][self.posx + 1] = 5
+            self.map[self.posy][self.posx + 2] = 2
+            self.posx += 1
+        #Muñeco, Caja-meta, Meta
+        elif self.map[self.posy][self.posx] == 1 and self.map[self.posy][self.posx + 1] == 6 and self.map[self.posy][self.posx + 2] == 4:
+            self.map[self.posy][self.posx] = 0
+            self.map[self.posy][self.posx + 1] = 5
+            self.map[self.posy][self.posx + 2] = 6
+            self.posx += 1
